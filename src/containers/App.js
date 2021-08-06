6169fc00b96b9c9d956ca0c606fff12e2143b439
EtienneRQT <etienne.riquet@sfr.fr>
2021-08-06 21:48:48 +0200

    final version of the robotfriends app for now

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { robots: [], searchfield: "" };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        const robots = users;
+        this.setState(() => ({ robots }));
+      });
+  }
+  onSearchChange = (event) => {
+    const searchfield = event.target.value;
+    this.setState(() => ({ searchfield }));
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    if (!robots.length) {
+      return <h1>Loeading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
